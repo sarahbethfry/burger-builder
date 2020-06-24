b02ba8d278390a61c6e9084c3881dde36f5f2ec5
Sarah Fry <sarahfry@Petrs-MacBook-Air.local>
2020-06-24 15:12:29 -0700

    Added reusable navigation component

--- src/components/Navigation/Toolbar/Toolbar.jsx
+++ src/components/Navigation/Toolbar/Toolbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import classes from "./Toolbar.css";
 import Logo from "../../Logo/Logo";
+import NavItems from "../NavItems/NavItems";
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>Menu</div>
     <Logo />
-    <nav>...</nav>
+    <NavItems />
   </header>
 );
 
