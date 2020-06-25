5dbd5d30b4d9eb44de80444cbf490171a34cf9ba
Sarah Fry <sarahfry@Petrs-MacBook-Air.local>
2020-06-25 11:36:42 -0700

    Responsive adjustments- logo

--- src/components/Navigation/Toolbar/Toolbar.jsx
+++ src/components/Navigation/Toolbar/Toolbar.jsx
@@ -6,7 +6,7 @@ import NavItems from "../NavItems/NavItems";
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>Menu</div>
-    <Logo />
+    <Logo height="80%" />
     <NavItems />
   </header>
 );
