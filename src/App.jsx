1186710b4d09f6c03359ed15c6aa615c8bcbea4a
Sarah Fry <sfry06@yahoo.com>
2020-09-09 12:24:36 -0700

    added lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
 import { Route, BrowserRouter, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import asyncComponent from "./hoc/asyncComponent/asyncComponent";
 
 import Layout from "./containers/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Checkout from "./containers/Checkout/Checkout";
-import Orders from "./containers/Orders/Orders";
-import Auth from "./containers/Auth/Auth";
+
 import Logout from "./containers/Auth/Logout";
 import * as actions from "./store/actions/index";
+
+const asyncCheckout = asyncComponent(() => {
+  return import("./containers/Checkout/Checkout");
+});
+
+const asyncOrders = asyncComponent(() => {
+  return import("./containers/Orders/Orders");
+});
+
+const asyncAuth = asyncComponent(() => {
+  return import("./containers/Auth/Auth");
+});
 
 class App extends Component {
   componentDidMount() {
@@ -18,17 +29,17 @@ class App extends Component {
     let routes = (
       <Switch>
         <Route path="/" exact component={BurgerBuilder} />
-        <Route path="/auth" component={Auth} />
+        <Route path="/auth" component={asyncAuth} />
         <Redirect to="/" />
       </Switch>
     );
     if (this.props.isAuth) {
       routes = (
         <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
+          <Route path="/checkout" component={asyncCheckout} />
+          <Route path="/orders" component={asyncOrders} />
           <Route path="/logout" component={Logout} />
-          <Route path="/auth" component={Auth} />
+          <Route path="/auth" component={asyncAuth} />
           <Route path="/" exact component={BurgerBuilder} />
           <Redirect to="/" />
         </Switch>
--- src/store/actions/order.jsx
+++ src/store/actions/order.jsx
@@ -28,7 +28,6 @@ export const submitOrder = (orderData, token) => {
     axios
       .post("/orders.json?auth=" + token, orderData)
       .then((response) => {
-        console.log(orderData, response);
         dispatch(checkoutSuccess(response.data.name, orderData));
       })
       .catch((error) => {
@@ -69,7 +68,6 @@ export const fetchOrders = (token, userId) => {
     const queryParams =
       "?auth=" + token + '&orderBy="userId"&equalTo="' + userId + '"';
     axios.get("/orders.json" + queryParams).then((res) => {
-      console.log(res);
       const fetchedOrders = [];
       for (let key in res.data) {
         fetchedOrders.push({ ...res.data[key], id: key });
