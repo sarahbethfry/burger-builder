7dcb90ea798ad70796a0ffa6571f8d38147eb5dd
Sarah Fry <sfry06@yahoo.com>
2020-09-09 13:10:55 -0700

    Removed lazy loading. Modern webpack wasnt working with it

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import React, { Component } from "react";
 import { Route, BrowserRouter, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import asyncComponent from "./hoc/asyncComponent/asyncComponent";
 
+import Orders from "./containers/Orders/Orders";
+import Checkout from "./containers/Checkout/Checkout";
+import Auth from "./containers/Auth/Auth";
 import Layout from "./containers/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 import Logout from "./containers/Auth/Logout";
 import * as actions from "./store/actions/index";
-
-const asyncCheckout = asyncComponent(() => {
-  return import("./containers/Checkout/Checkout");
-});
-
-const asyncOrders = asyncComponent(() => {
-  return import("./containers/Orders/Orders");
-});
-
-const asyncAuth = asyncComponent(() => {
-  return import("./containers/Auth/Auth");
-});
 
 class App extends Component {
   componentDidMount() {
@@ -29,17 +19,17 @@ class App extends Component {
     let routes = (
       <Switch>
         <Route path="/" exact component={BurgerBuilder} />
-        <Route path="/auth" component={asyncAuth} />
+        <Route path="/auth" component={Auth} />
         <Redirect to="/" />
       </Switch>
     );
     if (this.props.isAuth) {
       routes = (
         <Switch>
-          <Route path="/checkout" component={asyncCheckout} />
-          <Route path="/orders" component={asyncOrders} />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
           <Route path="/logout" component={Logout} />
-          <Route path="/auth" component={asyncAuth} />
+          <Route path="/auth" component={Auth} />
           <Route path="/" exact component={BurgerBuilder} />
           <Redirect to="/" />
         </Switch>
